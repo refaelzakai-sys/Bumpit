2b38d4540a9359c48f0b5ef0e052a06ffa0f5212
refaelzakai-sys <refaelzakai@gmail.com>
2026-05-07 07:58:46 +0300

    Remove unnecessary newline at the end of server.js

--- server.js
+++ server.js
@@ -45,4 +45,3 @@ app.post('/upload', upload.single('file'), async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
-
