c02eea88c1b77757bca925fda1f3876d86c04031
refaelzakai-sys <refaelzakai@gmail.com>
2026-05-07 07:55:44 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE_NAME = 'bump-luxury-v1';
-self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(['/'])));
-});
-
-self.addEventListener('fetch', (e) => {
-    e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
-});
